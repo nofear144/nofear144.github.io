cd16cf13510da5e6e4267f027616050f8e236285
nofear144 <nofear144@mail.ru>
2022-01-06 01:11:55 +0300

    Finished with functionality (wrong work by create task, created in wrong parent,need to fix),need to do css

--- main.js
+++ main.js
@@ -4,39 +4,95 @@ var listTodo = document.querySelector("#list_todo")
 
 addTodoBtn.addEventListener("click", function (e) {
     if (todoInput.value === '') return
-    createTodo.bind(listTodo,todoInput.value)()
+    createTodo.bind(listTodo, todoInput.value)()
+    todoInput.value = ""
 })
-
 
 
 function createTodo(value) {
     var label = document.createElement("h1")
     var todolist = document.createElement("div")
+    var headTodoContainer = document.createElement("div")
     var input = document.createElement('input')
-    var taskButton = document.createElement("button")
+    var taskBtn = document.createElement("button")
+    var deleteTodoBtn = document.createElement("button")
+    var finishedTasks = document.createElement("div")
+    var unfinishedTasks = document.createElement("div")
 
+    finishedTasks.className = 'finished_task'
+    unfinishedTasks.className = 'unfinished_task'
 
+    headTodoContainer.className = "head_todo_container"
     todolist.className = "todo"
-    taskButton.className = "add_btn"
+    taskBtn.className = "add_btn"
+    deleteTodoBtn.className = "add_btn"
     input.className = "add_inp_task"
-    taskButton.id = "add_btn"
     label.innerHTML = value
-    taskButton.innerHTML = "+"
+    taskBtn.innerHTML = "+"
+    deleteTodoBtn.innerHTML = "x"
 
     listTodo.appendChild(todolist)
-    todolist.appendChild(label)
+    todolist.appendChild(headTodoContainer)
+    headTodoContainer.appendChild(label)
+    headTodoContainer.appendChild(deleteTodoBtn)
     todolist.appendChild(input)
-    todolist.appendChild(taskButton)
+    todolist.appendChild(taskBtn)
+    todolist.appendChild(finishedTasks)
+    todolist.appendChild(unfinishedTasks)
 
-    taskButton.addEventListener("click",
-        createTask.bind(todolist, input)
+
+    taskBtn.addEventListener("click",
+        createTask.bind(todolist, input, finishedTasks, unfinishedTasks)
     )
+    deleteTodoBtn.addEventListener("click", deleteTodo)
+}
+function deleteTodo() {
+    var currentChild = this.parentNode
+    var currentTodo = currentChild.parentNode
+    var listTodo = currentTodo.parentNode
+
+    listTodo.removeChild(currentTodo)
 }
 
-function createTask(input) {
+function createTask(input, finishedTasks, unfinishedTasks) {
 
-    var task = document.createElement("div")
-    task.innerHTML = input.value
-    this.appendChild(task)
+    if (input.value === "") return
+    var taskContainer = document.createElement("div")
+    var taskName = document.createElement("div")
+    var deleteBtn = document.createElement("button")
+    var checkbox = document.createElement("input")
 
+    taskContainer.className = "task_container"
+    checkbox.type = "checkbox"
+    checkbox.innerHTML = "true"
+    deleteBtn.innerHTML = "delete"
+    taskName.innerHTML = input.value
+
+    this.appendChild(taskContainer)
+    taskContainer.appendChild(checkbox)
+    taskContainer.appendChild(taskName)
+    taskContainer.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener("click", deleteTask)
+    checkbox.addEventListener('click', switchStatus.bind(taskContainer, finishedTasks, unfinishedTasks, checkbox))
+
+    input.value = ""
 }
+
+function deleteTask() {
+    var task = this.parentNode
+    var todo = task.parentNode
+    todo.removeChild(task)
+}
+
+
+
+function switchStatus(finishedTasks, unfinishedTasks, checkbox) {
+    if (checkbox.checked) {
+        finishedTasks.appendChild(this)
+    } else {
+        unfinishedTasks.appendChild(this)
+    }
+}
+
+
